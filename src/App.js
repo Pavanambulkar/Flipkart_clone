e347d90820890947759730ea9905920f769233bd
pavan <pavanambulkar69@gmail.com>
2022-12-05 01:17:10 +0530

    product-page start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Products from './Components/Products';
 
 function App() {
   return (
     <div className="App">
-<Navbar/>   
+<Navbar/>  
+<Products/> 
  </div>
   );
 }
---
+++ src/Components/Products.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Products.css'
+const Products = () => {
+  return (
+    <div className='products'>
+     <div>
+        <img src="https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100" alt="grocery" />
+        <p>Grocery</p>
+     </div>
+     <div>
+        <img src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" alt="mobile" />
+        <p>Mobile</p>
+     </div>
+     <div>
+        <img src="https://rukminim1.flixcart.com/flap/128/128/image/c12afc017e6f24cb.png?q=100" alt="fashion" />
+        <p>Fashion</p>
+     </div>
+     <div>
+        <img src="https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100" alt="electronics" />
+        <p>Electronics</p>
+     </div>
+     <div>
+        <img src="https://rukminim1.flixcart.com/flap/128/128/image/ab7e2b022a4587dd.jpg?q=100" alt="home" />
+        <p>Home</p>
+     </div>
+     <div>
+        <img src="https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100" alt="appliances" />
+        <p>Appliances</p>
+     </div>
+     <div>
+        <img src="https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100" alt="travel" />
+        <p>Travel</p>
+     </div>
+     <div>
+        <img src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" alt="top-offers" />
+        <p>Top Offers</p>
+     </div>
+     <div>
+        <img src="https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100" alt="beauty" />
+        <p>Beauty, toys and more</p>
+     </div>
+     <div>
+        <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/05d708653beff580.png?q=100" alt="two-wheeler" />
+        <p>Two Wheelers</p>
+     </div>
+    </div>
+  )
+}
+
+export default Products
